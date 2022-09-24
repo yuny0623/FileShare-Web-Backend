db45aff21f9dc805cf03701e74c921307a32abf8
yuny0623 <overman522@gmail.com>
2022-09-24 17:05:18 +0900

    Add: index.html

--- src/main/java/com/example/imageGhost/Controller/UtilController.java
+++ src/main/java/com/example/imageGhost/Controller/UtilController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class UtilController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @GetMapping("/health-check")
     public String healthCheck(){
