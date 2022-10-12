8d111410cecb1ef741d4db55fdaa4184a355eb60
yuny0623 <overman522@gmail.com>
2022-10-12 21:48:57 +0900

    Add: create asymmetric key pair when springboot application is start.

--- src/main/java/com/example/imageGhost/ImageGhostApplication.java
+++ src/main/java/com/example/imageGhost/ImageGhostApplication.java
@@ -5,12 +5,20 @@ import com.example.imageGhost.Utils.ServerMetaInfoGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class ImageGhostApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImageGhostApplication.class, args);
-
 		// 서버 메모리 상에 비대칭키 키페어 올려두기
-		ServerMetaInfoGenerator.generateKeyPair();
+		HashMap<String, String> keyPair = ServerMetaInfoGenerator.generateKeyPair();
+
+		String publicKey = keyPair.get("publicKey");
+		String privateKey = keyPair.get("privateKey");
+
+		// 설정 파일에 등록
+		Config.setServerPublicKey(publicKey);
+		Config.setServerPrivateKey(privateKey);
 	}
 }
