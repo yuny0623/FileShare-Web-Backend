3e580ab9d8551c7b60e35405ceea8b18531e22b2
yuny0623 <overman522@gmail.com>
2022-10-13 16:16:29 +0900

    Test: asymmetric key pair generation test.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-ImageGhostServer 
+ImageGhostServer with SprintBoot 
 
-1. Mysql 연동  -> RDS 쓸까?  
-2. Docker 적용 -> 
-3. Redis 적용 -> 
-4. SSL/TLS 적용
-5. GCP 사용 
-6. 테스트 코드 작성하기 
-7. Client or thymeleaf 단 완성하기 
-8. Logger 사용하기 
+1. Mysql 연동  -> x
+2. Docker 적용 -> x
+3. Redis 적용 -> x
+4. SSL/TLS 적용 -> x
+5. GCP or AWS 사용  -> x
+6. 테스트 코드 작성하기 -> o   
+7. Client or thymeleaf 단 완성하기  -> x
+8. Logger 사용하기  -> o 
--- src/test/java/com/example/imageGhost/EncDecTest.java
+++ src/test/java/com/example/imageGhost/EncDecTest.java
@@ -9,16 +9,10 @@ import java.util.HashMap;
 
 public class EncDecTest {
 
-    /*
-		public key < - > private key 역방향 테스트
-		public key로 암호화한 plainText를 private key로 복호화할 수 있는지 테스트
-	 */
-
     @Test
     void 비대칭키_역방향_테스트1_공개키에서_비밀키(){
         // given
         HashMap<String, String> keyPair = ServerMetaInfoGenerator.generateAsymmetricKeyPair();
-
         String publicKey = keyPair.get("publicKey");
         String privateKey = keyPair.get("privateKey");
         String plainText = "ImageGhost";
@@ -34,11 +28,14 @@ public class EncDecTest {
 
     @Test
     void 비대칭키_키페어_생성_테스트(){
+        //given
+        HashMap<String, String> keyPair = ServerMetaInfoGenerator.generateAsymmetricKeyPair();
 
+        // when
+        String publicKey = keyPair.get("publicKey");
+
+        //then
+        Assertions.assertEquals(publicKey, ServerMetaInfoGenerator.SERVER_PUBLIC_KEY);
     }
 
-    @Test
-    void 대칭키_생성_테스트(){
-
-    }
 }
